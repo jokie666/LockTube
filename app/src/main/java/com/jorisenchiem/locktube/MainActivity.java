a44b2d20a0a78b5964dec84005976fa991fb634c
Chiem Janssen <chiemjanssen@home.nl>
2015-10-12 11:50:49 +0200

    Searchknop, XML layout

--- app/src/main/java/com/jorisenchiem/locktube/MainActivity.java
+++ app/src/main/java/com/jorisenchiem/locktube/MainActivity.java
@@ -3,11 +3,13 @@ package com.jorisenchiem.locktube;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        /* Searchbar item aanspreken er ermee kunnen werken
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        */
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
